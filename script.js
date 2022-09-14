0431aab6223b5417ec4aa31a0f185a6cde091893
ierdna89 <andreistrelciuc@gmail.com>
2022-09-14 16:45:58 +0300

    Created a 16x16 grid from divs using JavaScript

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-for (let i; i == 16; i++) {
+for (let i = 0; i <= 256; i++) {
   const container = document.getElementById("container");
   const divs = document.createElement('div');
-
+  divs.style.width = "10px";
+  divs.style.height = "10px";
+  container.appendChild(divs);
 }
 
 
