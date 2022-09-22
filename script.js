bfe806568997c402e3237f4d685d90edb1c1a9a6
ierdna89 <andreistrelciuc@gmail.com>
2022-09-22 16:08:42 +0300

    Implimented grid generating, drawing on grid, eraser function and reset grid

--- script.js
+++ script.js
@@ -1,53 +1,128 @@
-for (let i = 0; i <= 224; i++) {
-  const container = document.getElementById("container");
-  const divs = document.createElement('div');
-  divs.style.width = "40px";
-  divs.style.height = "40px";
-  divs.style.boxSizing = "border-box";
-  divs.style.border = "0.1px solid black";
-  divs.addEventListener("touchstart", () => {
-    divs.style.backgroundColor = "pink";
-  });
-  container.appendChild(divs);
+let slider = document.getElementById("myRange");
+let output = document.getElementById("gridSize");
+let isEraserButtonClicked = false;
+
+output.innerText = `${slider.value} x ${slider.value}`;
+
+slider.oninput = () => {
+  output.innerText = `${slider.value} x ${slider.value}`;
+}
+
+slider.addEventListener("click", drawGrid);
+
+function drawDefaultGrid() {
+  for (let i = 0; i <= 256 - 1; i++) {
+    const container = document.getElementById("gridContainer");
+    const divs = document.createElement('div');
+    divs.style.width = "31.25px";
+    divs.style.height = "31.25px";
+    container.style.width = "500px";
+    divs.style.boxSizing = "border-box";
+    divs.style.border = "0.1px solid black";
+    divs.setAttribute("class", "gridDivs");
+    container.appendChild(divs);
+  }
+
+  document.getElementById("myRange").value = "16";
+  console.log(isEraserButtonClicked);
+
+  drawOnBoard();
+}
+
+drawDefaultGrid();
+
+function drawGrid() {
+  clearGrid();
+
+  let gridSquaresXtraLarge = parseInt(output.innerText, 10);
+
+  function calcTotalGrid(x) {
+    return x = gridSquaresXtraLarge * gridSquaresXtraLarge;
+  }
+
+  function calcSquareDimensions(x) {
+    x = 500 / gridSquaresXtraLarge;
+    return x.toPrecision(4);;
+  }
+
+  function getGridDimension(x) {
+    x = calcSquareDimensions() * gridSquaresXtraLarge + 0.4;
+    return x.toFixed(2);
+  }
+
+  for (let i = 0; i <= calcTotalGrid() - 1; i++) {
+    const container = document.getElementById("gridContainer");
+    const divs = document.createElement('div');
+    divs.style.width = `${calcSquareDimensions()}px`;
+    divs.style.height = `${calcSquareDimensions()}px`;
+    container.style.width = `${getGridDimension()}px`;
+    divs.style.boxSizing = "border-box";
+    divs.style.border = "0.1px solid black";
+    divs.setAttribute("class", "gridDivs");
+    container.appendChild(divs);
+  }
+  drawOnBoard();
 }
 
 
-let isDown = false;
+const button = document.getElementById("btn-Eraser");
+button.addEventListener("click", () => {
+  button.classList.toggle("btnClicked");
+  if (isEraserButtonClicked === false) {
+    isEraserButtonClicked = true;
+    console.log(isEraserButtonClicked);
+  }
+  else {
+    isEraserButtonClicked = false;
+    console.log(isEraserButtonClicked);
+  }
+});
 
-const divs2 = document.querySelector("container");
-for (const divs2 of container.children) {
-  divs2.addEventListener("touchmove", () => {
-    if (isDown === true) {
-      divs2.style.backgroundColor = "pink";
-    }
+
+const clearGridButton = document.getElementById("btn-Reset");
+clearGridButton.addEventListener("click", () => {
+  clearGrid();
+  drawGrid();
+});
+
+function clearGrid() {
+  let gridDivs = document.getElementById('gridContainer');
+  while (gridDivs.firstChild) {
+    gridDivs.removeChild(gridDivs.firstChild);
+  }
+}
+
+
+//drawing on the board
+function drawOnBoard() {
+  let isDown = false;
+
+  const divs2 = document.querySelector("gridContainer");
+  for (const divs2 of gridContainer.children) {
+    divs2.addEventListener("mousedown", () => {
+      if (isEraserButtonClicked === false) {
+        divs2.style.backgroundColor = "coral";
+      }
+      else {
+        divs2.style.backgroundColor = "white";
+      }
+    });
+
+    divs2.addEventListener("mouseover", () => {
+      if (isDown === true && isEraserButtonClicked === false) {
+        divs2.style.backgroundColor = "coral";
+      }
+      else if (isDown === true) {
+        divs2.style.backgroundColor = "white"
+      }
+    });
+  };
+
+  document.addEventListener('mousedown', () => {
+    isDown = true;
   });
-};
 
-document.addEventListener('touchstart', () => {
-  isDown = true;
-});
-
-document.addEventListener('touchend', () => {
-  isDown = false;
-});
-
-
-// const divs2 = document.querySelector("container");
-// for (const divs2 of container.children) {
-//   divs2.addEventListener("click", () => {
-//     divs2.style.backgroundColor = "pink";
-//   });
-//   divs2.addEventListener("mouseover", () => {
-//     divs2.style.backgroundColor = "pink";
-//   });
-
-// };
-
-
-
-// const divs3 = document.querySelector("container");
-// for (const divs3 of container.children) {
-//   divs3.addEventListener("click", () => {
-//     divs3.style.backgroundColor = "gray";
-//   });
-// };
+  document.addEventListener('mouseup', () => {
+    isDown = false;
+  });
+}
